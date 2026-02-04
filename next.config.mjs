01e52f032ec11073a276587a69d54896fe61c680
indigoandlavender <happy@indigoandlavender.love>
2026-02-04 17:17:11 +0100

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "res.cloudinary.com",
-      "drive.google.com",
-      "lh3.googleusercontent.com",
-      "images.unsplash.com",
-      "eedmcvirivigkuzohafv.supabase.co",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "drive.google.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "eedmcvirivigkuzohafv.supabase.co",
+      },
     ],
   },
 };
